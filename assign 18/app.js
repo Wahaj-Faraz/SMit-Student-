a9168257c04b8acbe0a424c3830c4c217aa1857d
Wahaj-Faraz <157166947+Wahaj-Faraz@users.noreply.github.com>
2024-03-24 07:35:32 +0500

    Fourth Commit

---
+++ assign 18/app.js
@@ -0,0 +1,180 @@
+        //         Question 01
+        //  var multiArray=[[1],[2],[3]];
+
+        //         Question 02
+        // var matrix = [
+        //         [0,1,2,3] ,
+//         [1,0,1,2],
+//         [2,1,0,1]
+//                 ];
+//         document.write(matrix[0][0] + " "); 
+//     document.write(matrix[0][1] + " ");
+//  document.write(matrix[0][2] + " ");
+//     document.write(matrix[0][3]);
+//     document.write("<br />");
+//     document.write(matrix[1][0] + " ");
+//     document.write(matrix[1][1]+ " ");
+//     document.write(matrix[1][2] + " ");
+//     document.write(matrix[1][3]);
+//     document.write("<br />");
+//     document.write(matrix[2][0] + " ");
+//     document.write(matrix[2][1] + " ");
+//     document.write(matrix[2][2] + " ");
+//     document.write(matrix[2][3]);
+//     document.write("<br />");
+//     document.write(matrix[3][0] + " ");
+//     document.write(matrix[3][1] + " ");
+//     document.write(matrix[3][2] + " ");
+//     document.write(matrix[3][3]);
+
+        // Question 03
+// for(var i=1;i<=10;i++){
+//         document.write(i + "<br />")
+// };
+
+        // Question 04
+// var tableNumber = +prompt('Enter the table number: ');
+//  if (!isNaN(tableNumber)) {
+//     var tableLength = +prompt('Enter the length of the table: ');
+//    if (!isNaN(tableLength)) {
+//          for (var i = 1; i <= tableLength; i++) {
+//             document.write(`${tableNumber} * ${i}=${tableNumber * i}` + "<br />");
+//         }
+//         document.write('</table>');
+//     } else {
+//         document.write('<p>Please enter a valid number for the length of the table.</p>');
+//     }
+// } else {
+//     document.write('<p>Please enter a valid number for the table number.</p>');
+// };
+
+        // Quesrrion 05
+//         var fruits = ["apple", "banana", "mango", "orange", "strawberry"];
+//    for (let i = 0; i < fruits.length; i++) {
+//             document.write(fruits[i] + "<br />");
+//         }
+//         document.write("<br /><br />")
+//         for (let a= 0; a < fruits.length; a++) {
+//                 document.write("Element at Index " + a + " is " + fruits[a] + "<br />");
+//             };
+
+        // Question 06
+        //    (a)
+        // document.write("Counting: ")
+        // for(var a=1;a<=15;a++){
+        //         document.write( a + ",")
+        // };
+
+        //   (b)
+        // document.write("Reverse Counting: ")
+        // for(var a=10;a>=1;a--){
+        //          document.write( a + ",")
+        //  };
+
+        //   (c)
+// document.write("Even: ");
+// for(var i=1;i<=20;i++){
+//         if(i%2==0){
+//                 document.write(i + ",")
+//         }
+// };
+
+        //  (d)
+// document.write("Odd: ");
+// for(var i=1;i<=20;i++){
+//         if(i%2==1){
+//                 document.write(i + ",")
+//         }
+// };
+
+        //    (e)
+        // var start = 2;
+        // var commonDifference = 2;
+        //   var numTerms = 10;
+        //   document.write("Series: ");
+        // for (var i = 1; i <= numTerms; i++) {
+        //     var currentTerm = start + (i - 1) * commonDifference;
+        //     document.write(currentTerm + "k ,");
+        // };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
