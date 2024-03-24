dd4db014fd88d9fa1021de89b0f75cf2e9b91718
Wahaj-Faraz <157166947+Wahaj-Faraz@users.noreply.github.com>
2024-03-24 07:34:58 +0500

    Third Commit

---
+++ assign 17/chap 12-13/app.js
@@ -0,0 +1,210 @@
+// Question 01
+//         var userInput = prompt("Enter a character:");
+//         var charCode = userInput.charCodeAt(0);
+//         if (charCode >= 65 && charCode <= 90) {
+//         document.write(`${userInput} is an uppercase letter.`);
+//         } else if (charCode >= 97 && charCode <= 122) {
+//         document.write(`${userInput} is a lowercase letter.`);
+//         } else if (!isNaN(userInput)) {
+//        document.write(`${userInput} is a digit.`);
+//        } else {
+//          document.write(`${userInput} is not a digit, uppercase letter, or lowercase letter.`);
+//        }
+
+// Question 02
+// var num1 = +prompt("Enter the first integer:");
+// var num2 = +prompt("Enter the second integer:");
+// if (num1 > num2) {
+//     document.write(num1 + ' is larger.');
+// } else if (num2 > num1) {
+//     document.write(num2 + ' is larger.');
+// } else {
+//     document.write("Both integers are equal.");
+// }
+
+// Question 03
+// var number=+prompt("Enter The Number...");
+// if(number>0){
+// document.write(number + " Is Positive Number")
+// }
+// else if(number<0){
+// document.write(number + " Is Negative Number")
+// }else if(number==0){
+// document.write(number + " Is Zero")
+// }else{
+// document.write("Invalid Input")
+// };
+
+// Question 04
+// var string=prompt("Enter The Character...");
+// if(string=="a","e","i","o","u","A","E","I","O","U"){
+//     document.write("True")
+// }else{
+//     document.write("False")
+// };
+
+// Question 05
+// let correctPassword = "123456789";
+// let userPassword = prompt("Enter your password: ");
+// if (!userPassword){
+//     document.write("Please enter your password.");
+// } else {
+//     if (userPassword === correctPassword) {
+//         document.write("Correct! The password you entered matches the original password.");
+//     } else {
+//         document.write("Incorrect password.");
+//     }
+// }
+
+// Question 06
+//     var greeting;
+//     var hour = 13;
+//     if (hour < 18) {
+//    document.write( greeting = "Good day");
+//     }else{
+//     document.write(greeting = "Good evening");
+//     };
+
+// Question 07
+// var time = prompt("Enter time in 24-hour clock format: ");
+// if (time >= 0 && time <= 1159) {
+//     document.write("Good Morning!");
+// } else if (time >= 1200 && time <= 1759) {
+//     document.write("Good Afternoon!");
+// } else if (time >= 1800 && time <= 2359) {
+//     document.write("Good Evening!");
+// } else {
+//     document.write  ("Invalid input. Please enter time in 24-hour clock format.");
+// };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ assign 17/chap14-16/app.js
@@ -0,0 +1,249 @@
+// Question 01
+//var studentName=[];
+
+// Question 02
+//  var studentName=[];
+
+// Question 03
+//var nameArray=["John","Doe","Mark"];
+
+// Question 04
+//var markArray=[45,82,19];
+
+// Question 05 
+//var booleanArray=[True,False,False];
+
+// Question 06
+//var mixArray = [false, 96, "Wahaj", false, 8.109, "Faraz"];
+
+// Question 07
+// var qualifications = ["SSC", "HSC", "BCS", "BS", "BCOM", "MS", "M. Phil.", "PhD"];
+// document.write("<h1>Qualifications</h1>")
+// document.write("1) " + qualifications[0] + "<br />");
+// document.write("2) " + qualifications[1] + "<br />");
+// document.write("3) " + qualifications[2] + "<br />");
+// document.write("4) " + qualifications[3] + "<br />");
+// document.write("5) " + qualifications[4] + "<br />");
+// document.write("6) " + qualifications[5] + "<br />");
+// document.write("7) " + qualifications[6] + "<br />");
+// document.write("8) " + qualifications[7] + "<br />");
+
+// Question 08
+// var studName=["Michael","John","Tony"];
+// var studMarks=[320,230,480];
+// var total=500;
+// var per_0=studMarks[0]/total*100;
+// var per_1=studMarks[1]/total*100;
+// var per_2=studMarks[2]/total*100;
+// document.write(" <b>Score Of " + studName[0] + " is " + studMarks[0] + " . Percentage: " + per_0 + "% </b><br />");
+// document.write(" <b>Score Of " + studName[1] + " is " + studMarks[1] + " . Percentage: " + per_1 + "% </b><br />");
+// document.write(" <b>Score Of " + studName[2] + " is " + studMarks[2] + " . Percentage: " + per_2 + "% </b><br />");
+
+// Question 09
+//var colorArray=[" Red "," Blue "," Green "," Yellow "];
+//document.write(colorArray);
+//   [a]
+//var startcolor=prompt("Enter The Color...");
+//colorArray.unshift(startcolor);
+//  [b]
+//document.write(colorArray);
+// var endcolor=prompt("Enter The Color...");
+// colorArray.push(endcolor);
+// document.write(colorArray);
+// [c]
+// var oneColor=prompt("Enter The Color");
+// var twoColor=prompt("Enter The Color");
+// colorArray.unshift(oneColor,twoColor);
+// document.write(colorArray);
+// [d]
+//colorArray.shift();
+//document.write(colorArray);
+// [e]
+// colorArray.pop();
+// document.write(colorArray);
+// [f]
+// var index=prompt("Enter The Index Number");
+// var Color=prompt("Enter The Color");
+// colorArray[index]=Color;
+// document.write(colorArray);
+// [g]
+// var index=prompt("Enter The Index Number");
+//  colorArray.splice(index,1)[0];
+//  document.write(colorArray);
+
+// Question 10
+// var studMarks=[320,230,480,120];
+// document.write("Scores Of Students: " + studMarks)
+// studMarks.sort((a,b)=>a-b);
+// document.write("<br />Ordered Scores Of Students: " + studMarks);
+
+// Question 11
+// var cities=["Karachi","Lahore","Islamabad","Quetta","Peshawar"];
+// document.write("Cities List: <br/>")
+// document.write(cities + "<br /><br />");
+// var selectedcities = cities.slice(2,4);
+// document.write( "Selected Cities List :<br />" + selectedcities + "<br />");
+
+// Question 12
+// var array = ['This', 'is', 'my' , 'cat'];
+// var joinedString = array.join();
+// document.write("Array: <br />")
+// document.write(joinedString); 
+// var joinedStringWithDash = array.join(' ');
+// document.write("<br />String <br />" + joinedStringWithDash); 
+
+// Question 13
+// var stack = ['Keyboard','Mouse','Printer','Monitor'];
+// document.write("Devices: <br />" + stack + "<br />")
+// document.write("Out: <br/>" + stack.pop()); 
+// document.write("<br />Out: <br/>" +stack.pop()); 
+// document.write("<br />Out: <br/>" +stack.pop()); 
+// document.write("<br />Out: <br/>" +stack.pop());
+
+// Question 15
+// var manufacturersArray = ["Apple", "Samsung", "Motorola", "Nokia", "Sony", "Haier"];
+// document.write("<select>");
+// for (var i = 0; i < manufacturersArray.length; i++) {
+//     document.write("<option value='" + manufacturersArray[i] + "'>" + manufacturersArray[i] + "</option>");
+// }
+// document.write("</select>");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
